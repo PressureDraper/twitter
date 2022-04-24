40592b2f77a2cde95b1ea5cb7bf8b30548ddf938
PressureDraper <warmarines00@hotmail.com>
2022-04-23 19:28:59 -0500

    step 2: writing functional code for making it pass

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,7 +3,7 @@ const User = require("./../../app/models/user.js")
 describe("Unit Test for User Class", () => {
     test('1) Creating user object', () => {
         //Escribimos aqui el código que queremos usar tal cual
-        const user = new User("PressureDraper", "Sahib", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "PressureDraper", "Sahib", "Bio", "dateCreated", "lastUpdated")
 
         //Validamos que el código funcione de la forma esperada
         expect(user.id).toBe(1)
